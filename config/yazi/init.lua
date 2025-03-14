2c8b0c376c1a2a5a9da0baf60bd3b642ab94e157
Alexander Nole <cnole_azabache@outlook.com>
2025-03-13 20:06:17 -0500

    feat: add yazi

---
+++ config/yazi/init.lua
@@ -0,0 +1,2 @@
+require("eza-preview"):setup({ evel = 1 })
+require("starship"):setup({ config_file = "~/.config/starship/starship.toml" })
---
+++ config/yazi/install-plugins.sh
@@ -0,0 +1,5 @@
+ya pack --add GrzegorzKozub/mdcat
+ya pack --add ahkohd/eza-preview
+ya pack --add ndtoan96/ouch
+ya pack --add Reledia/hexyl
+ya pack --add Lil-Dank/lazygit
